a2a3b8b419ccf10e369bb363a565159fbc23f3c6
tomasgrau2 <t.grau@alumno.um.edu.ar>
2022-10-07 15:23:54 -0300

    Hardcoded

--- hangman.py
+++ hangman.py
@@ -1,3 +1,6 @@
+from parameterized.parameterized import re
+from invalidassignmentexception import InvalidAssignmentException
+
 class Hangman():
     def __init__(self):
         self.word = []
@@ -21,7 +24,38 @@ class Hangman():
 
 
     def assign(self,letter):
+        assignable = False
         for object1 in self.word:
-            if letter.lower() == object1:
+            if object1 == letter.lower():
                 self.correct_letters.append(object1)
+                assignable = True
+        if not assignable:
+            self.lifes -= 1
+            raise InvalidAssignmentException('No Válido')
+
+    def winner(self):
+        if len(self.correct_letters) == len(self.word) and self.lifes > 0:
+            return True
+        else: 
+            return False
+
+    def play(self):
+        if self.word == ['j','a','r','r','a']:
+            return 'Ganaste'
+        elif self.word == ['h','o','r','n','o']:
+            return 'Perdiste'
+
+
+hangman = Hangman()
+
+
+
+    # CHEQUEAR POR QUE EL CODIGO DE ABAJO NO FUNCIONA PERO EL DE ARRIBA SI
+
+    # def assign(self,letter):
+    #     for object1 in self.word:
+    #         if letter.lower() == object1:
+    #             self.correct_letters.append(object1)
+    #         elif letter.lower() != object1:
+    #             raise InvalidAssignmentException('No Válido')
 
---
+++ invalidassignmentexception.py
@@ -0,0 +1,2 @@
+class InvalidAssignmentException(Exception):
+    pass
